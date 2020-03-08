3feeb66f14e30b7c65e5e7cc14f64505bb28639d
yxsong <yxsong@cug.edu.cn>
2020-03-08 20:58:36 +0800

    整合代码，待改进

--- NDVI_task04.js
+++ NDVI_task04.js
@@ -21,53 +21,72 @@ var col = NDVICollection.map(function(img){
     .copyProperties(img,['system:time_start','system:time_end']);
     });
 
-// grouped by month 
-var months = ee.List([11,12,1,2]);
-
-var byMonth = ee.ImageCollection.fromImages(
-    months.map(function (m) {
-        return col.filterDate('2019-11-01',now).filter(ee.Filter.calendarRange(m, m, 'month'))
-                    .select('NDVI').mean()
-                    .set('month', m);
-    }));
-
-var meanNDVI = byMonth.reduce(ee.Reducer.mean());
-var     mask = meanNDVI.gt(0.1);
-
+var maskNDVI = function(image){
+  // grouped by month 
+  var months = ee.List([11,12,1,2]);
+  var byMonth = ee.ImageCollection.fromImages(
+      months.map(function (m) {
+          return col.filterDate('2019-11-01',now).filter(ee.Filter.calendarRange(m, m, 'month'))
+                      .select('NDVI').mean()
+                      .set('month', m);
+      }));
+  
+  var meanNDVI = byMonth.reduce(ee.Reducer.mean());
+  var     mask = meanNDVI.gt(0.1);
+  return image.updateMask(mask);
+};
 var addTime = function(image) {
   // Scale milliseconds by a large constant to avoid very small slopes
   // in the linear regression output.
   return image.addBands(image.metadata('system:time_start').divide(1e18));
 };
 
-var collection = col.map(addTime);
+var addTimeClip = function(image) {
+  return image.addBands(image.metadata('system:time_start')
+    .divide(1000 * 60 * 60 * 24 * 365)).clip(regions);
+};
+var collection = col.map(addTimeClip).map(maskNDVI);
 var taskNdviCol = collection.filterDate('2019-11-01',now);
 
 var linearFit = taskNdviCol.select(['system:time_start', 'NDVI'])
   .reduce(ee.Reducer.linearFit());  
+var slope = linearFit.select(['scale']);
 
-// Display the results.
-Map.centerObject(regions,2);
-Map.addLayer(linearFit.clip(regions), 
-  {min: [0, 150, 0], max: [-0.9, 1700, 1], bands: ['scale',  'offset', 'scale']}, 'fit');
+print('Projection, crs, and crs_transform:', slope.projection());
+print('Scale in meters:', slope.projection().nominalScale());
 
-function exportImgCol(imgCol,str) {
-      var indexList = imgCol.reduceColumns(ee.Reducer.toList(), ["system:index"]).get("list");
-      indexList.evaluate(function(indexs) {
-        for (var i=0; i<indexs.length; i++) {
-          var img0      = imgCol.filter(ee.Filter.eq("system:index", indexs[i])).first().updateMask(mask);
-          var desc_name = img0.get('system:index').getInfo();
-            Export.image.toDrive({
-            image      : img0.clip(regions),
-            description: str + desc_name,
-            folder     : 'Africa_Locust',
-            region     : regions.geometry().bounds(),
-            scale      : 1000,
-            crs        : "EPSG:4326",
-            maxPixels  : 1e13
-          });
-        }
-      });
-    }
+// Combine the mean and standard deviation reducers.
+var reducers = ee.Reducer.mean().combine({
+  reducer2: ee.Reducer.minMax(),
+  sharedInputs: true
+});
 
-exportImgCol(ee.ImageCollection(linearFit.updateMask(mask)),'regress_');
+
+// Use the combined reducer to get the mean and SD of the image.
+var stats = slope.reduceRegion({
+  reducer: reducers,
+  geometry: regions,
+  scale: 111319.49079327357
+});
+
+print(stats);
+var min_value = ee.Number(stats.get("scale_min"));
+var max_value = ee.Number(stats.get("scale_max"));
+
+var red_colorBrewer = ['67000d','ef3b2c','fcbba1','fff5f0'];
+var visParam = {
+  min: -6,
+  max: 3,
+  palette: red_colorBrewer
+};
+Map.centerObject(regions,3);
+Map.addLayer(slope, visParam, 'Slope NPP Change');
+
+// Export the image, specifying scale and region.
+Export.image.toDrive({
+  image: slope,
+  description: 'NDVI_Slope_Monthly',
+  maxPixels:1e13,
+  scale: 1000,
+  region: regions.geometry()
+});
